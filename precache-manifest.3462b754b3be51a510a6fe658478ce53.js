644b39be9c7cbdc9cec3163a653026665e140e68
Salem Alshehri <sal5m@hotmail.com>
2019-02-20 16:05:06 +0300

    Updates

---
+++ precache-manifest.3462b754b3be51a510a6fe658478ce53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa572a2271c48a67ba9c",
+    "url": "/Project-2/static/css/main.893e9a72.chunk.css"
+  },
+  {
+    "revision": "fa572a2271c48a67ba9c",
+    "url": "/Project-2/static/js/main.fa572a22.chunk.js"
+  },
+  {
+    "revision": "5f9573e2d19e10f10602",
+    "url": "/Project-2/static/js/runtime~main.5f9573e2.js"
+  },
+  {
+    "revision": "6ea6c4b890d5bb62bafe",
+    "url": "/Project-2/static/js/2.6ea6c4b8.chunk.js"
+  },
+  {
+    "revision": "cdb3976cfd50969823084429d3ddbac6",
+    "url": "/Project-2/static/media/ToDoList.cdb3976c.jpg"
+  },
+  {
+    "revision": "cb7748a49bb5971a7ab38bfd4eec75df",
+    "url": "/Project-2/index.html"
+  }
+];
